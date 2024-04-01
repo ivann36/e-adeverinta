a53cd9448deb4fb0b348b2d2b1d85f2d4157bf00
Ivan <113459000+ivann36@users.noreply.github.com>
2024-04-01 18:47:24 +0300

    Initialized TypeOrm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GoogleAuthModule } from './google-auth/google-auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [GoogleAuthModule],
+  imports: [
+    GoogleAuthModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db/sql.sqlite',
+      synchronize: true,
+      entities: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
