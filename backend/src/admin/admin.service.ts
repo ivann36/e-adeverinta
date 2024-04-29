7f83c37c0f641352b3b09bc7e0ebdf56466d724f
Ivan <113459000+ivann36@users.noreply.github.com>
2024-04-29 15:03:39 +0300

    Updated admin refresh token functionality

--- backend/src/admin/admin.service.ts
+++ backend/src/admin/admin.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Admin } from './admin.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AdminService {
@@ -12,5 +13,24 @@ export class AdminService {
 
   findByName(name: string) {
     return this.adminRepository.findOneBy({ name: name });
+  }
+
+  async storeRefreshToken(name: string, refreshToken: string) {
+    try {
+      const hash = await bcrypt.hash(refreshToken, 4);
+      const admin = await this.adminRepository.findOneBy({ name: name });
+      admin.refreshToken = hash;
+      this.adminRepository.save(admin);
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException(
+        'Error while storing refresh token',
+      );
+    }
+  }
+
+  async getRefreshTokenHash(username: string): Promise<string> {
+    const admin = await this.adminRepository.findOneBy({ name: username });
+    return admin.refreshToken;
   }
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { AdminService } from 'src/admin/admin.service';
 import { PayloadDto } from './dtos.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -17,21 +18,34 @@ export class AuthService {
     pass: string,
   ): Promise<{ access_token: string; refresh_token: string }> {
     const user = await this.usersService.findByName(username);
-    if (user?.password !== pass) {
+    const isValidPass = await bcrypt.compare(pass, user.password);
+    if (!isValidPass) {
       throw new UnauthorizedException();
     }
     const payload: PayloadDto = { sub: user.id, username: user.name };
+    const refresh_token = await this.jwtService.signAsync(payload, {
+      expiresIn: '7d',
+    });
+    await this.usersService.storeRefreshToken(user.name, refresh_token);
     return {
       access_token: await this.jwtService.signAsync(payload),
-      refresh_token: await this.jwtService.signAsync(payload, {
-        expiresIn: '7d',
-      }),
+      refresh_token: refresh_token,
     };
   }
 
   async refreshAccessToken(refreshToken: string): Promise<any> {
     try {
       const payload: any = await this.jwtService.verifyAsync(refreshToken);
+      const refreshTokenHash = await this.usersService.getRefreshTokenHash(
+        payload.username,
+      );
+      const isValidRefreshToken = await bcrypt.compare(
+        refreshTokenHash,
+        refreshToken,
+      );
+      if (!isValidRefreshToken) {
+        throw new UnauthorizedException();
+      }
       return {
         access_token: await this.jwtService.signAsync({
           sub: payload.sub,
